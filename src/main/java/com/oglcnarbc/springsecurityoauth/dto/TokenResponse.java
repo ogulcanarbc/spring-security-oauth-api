cf8c733e5a21f122fa0e6bdb0bfab62f790a33d6
Oğulcan Arabacı <ogulcan.a81@hotmail.com>
2019-04-29 16:05:10 +0300

    Update TokenResponse.java

--- src/main/java/com/oglcnarbc/springsecurityoauth/dto/TokenResponse.java
+++ src/main/java/com/oglcnarbc/springsecurityoauth/dto/TokenResponse.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//son kullanıcıya (client) bu token bilgileri dönecek
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
